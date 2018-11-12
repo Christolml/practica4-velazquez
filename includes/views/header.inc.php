45a33cab673548d910a0944c6c3a65d489a0a12a
christolml <christo_9722@hotmail.com>
2018-11-12 18:31:31 +0000

    Initial commit

---
+++ includes/views/header.inc.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+
+
+
+
+
+
+<head>
+	<title>Simple cat&aacute;logo </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="<?php echo $url;?>assets/css/bootstrap.min.css">
+
+
+
+      <meta name="description" content="PWA de la práctica #4 de la materia mantenimiento de software hecha por Christopher Velázquez.">
+      <meta name="author" content="Christopher Velázquez">
+
+      <meta name="theme-color" content="#2F3BA2">
+      <meta name="MobileOptimized" content="width">
+      <meta name="HandheldFriendly" content="true">
+      <meta name="apple-mobile-web-app-title" content="Asistencias">
+      <meta name="application-name" content="Asistencias"/>
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+      <meta name="msapplication-TileImage" content="img/icon_128.png">
+      <link rel="shortcut icon" type="image/png" href="./img/Christolml.png">
+      <link rel="apple-touch-icon" href="./Christolml.png">
+      <link rel="apple-touch-startup-image" href="./Christolml.png">
+      <link rel="manifest" href="/~ubuntu/manifest.json">
+    
+      <title>Práctica #4</title>
+
+
+
+<head>
+	
+<body class="page">
+<header>
+    <nav role="navigation">
+        <div class="container">
+            <div class="page-header">
+                <h1>Direcci&oacute;n de sistemas <small>Cat&aacute;logo de personas</small></h1>
+            </div>
+        </div>
+    </nav>        
+</header>
+<hr />
+<!-- end of header -->
+
+
+	<!-- div class="container">
+
+		<h1>Simple Blog - MySqli</h1>
+		<h3><a href="create.php">Add Post</a></h3><hr / -->
